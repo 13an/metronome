17deeef870cd11da17a4e1c5b8853654568742bf
13an <banjoyatakumi@gmail.com>
2023-04-30 17:21:33 +0900

    initial commit

---
+++ Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by 13an on 2023/04/30.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Metronome/MetronomeView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+struct ContentView: View {
+    // UI表示に必要なパラメーター
+    @State private var bpm: Int = 60
+    @State private var isPlaying: Bool = false
+
+    // コントローラーのインスタンス
+    private var hapticController = MetronomeController()
+
+    var body: some View {
+        VStack {
+            Text("BPM")
+                .font(.system(size: 20))
+
+            // BPMを表示
+            Text(String(bpm))
+                .font(.system(size: 68))
+
+            // "+"ボタン: タップするとBPMを1増やす
+            Button(action: {
+                    self.bpm += 1
+            }, label: {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60.0, height: 60.0)
+            })
+            .disabled(isPlaying)
+
+            // 再生・停止ボタン
+            if isPlaying == false {
+                Button(action: {
+                    hapticController.bpm = Double(self.bpm)
+                    hapticController.play()
+                    isPlaying = true
+                }) {
+                    Text("Start")
+                        .font(.system(size: 60))
+                }
+            } else {
+                Button(action: {
+                    hapticController.stop()
+                    isPlaying = false
+                }) {
+                    Text("Stop")
+                        .font(.system(size: 60))
+                }
+            }
+        }
+    }
+}
