152c8153952f19a389b530a54f0cc52017c29b47
13an <banjoyatakumi@gmail.com>
2023-04-30 23:21:57 +0900

    available for long press to control bpm

--- Metronome/MetronomeView.swift
+++ Metronome/MetronomeView.swift
@@ -5,6 +5,10 @@ struct MetronomeView: View {
     @State private var bpm: Int = 60
     @State private var isPlaying: Bool = false
 
+    // experiment long pressable button
+    @State private var longPressed = false
+    @State private var timer: Timer?
+
     // instances of ViewController
     private var metronomeController = MetronomeController()
 
@@ -17,32 +21,67 @@ struct MetronomeView: View {
             Text(String(bpm))
                 .font(.system(size: 68))
 
-            // bpm-controller
+            // bpm-control: experiment long pressable button
             HStack {
                 // slow-down-button
                 Button(action: {
+                    if longPressed {
+                        longPressed = false
+                    } else {
                         self.bpm -= 1
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        self.longPressed = false
+                        timer?.invalidate()
+                        timer = nil
+                    }
                 }, label: {
                     Image(systemName: "minus.circle")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 48.0, height: 48.0)
                 })
+                .simultaneousGesture(
+                    LongPressGesture().onEnded{ _ in
+                        self.longPressed.toggle()
+                        self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (Timer) in
+                            self.bpm -= 1
+                        })
+                    }
+                )
                 .disabled(isPlaying)
                 .padding(.trailing, 24)
                 
                 // speed-up-button
                 Button(action: {
+                    if longPressed {
+                        longPressed = false
+                    } else {
                         self.bpm += 1
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        self.longPressed = false
+                        timer?.invalidate()
+                        timer = nil
+                    }
                 }, label: {
                     Image(systemName: "plus.circle")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 48.0, height: 48.0)
                 })
+                .simultaneousGesture(
+                    LongPressGesture().onEnded{ _ in
+                        self.longPressed.toggle()
+                        self.timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (Timer) in
+                            self.bpm += 1
+                        })
+                    }
+                )
                 .disabled(isPlaying)
+
             }
-            
+
 
             // play/stop button
             if isPlaying == false {
@@ -67,7 +106,7 @@ struct MetronomeView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MetronomeView_Previews: PreviewProvider {
     static var previews: some View {
         MetronomeView()
     }
